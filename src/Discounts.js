8671bc83d060916085b61b3074748a7bf4ceb3b4
tamara-danilova2105 <sap1028@mail.ru>
2022-02-13 13:44:55 +0400

    your message

--- src/Discounts.js
+++ src/Discounts.js
@@ -1,7 +1,49 @@
+import { useState } from 'react'
+import { dataDiscounts } from './dataDiscounts.js'
+import banner from './photo/banner.jpg'
+
 function Discounts(){
+
+    const [discount, setDiscount] = useState(0)
+    const {image, header, text} = dataDiscounts[discount]
+
+    const previos = () => {
+        setDiscount((discount => {
+            discount --
+            if(discount < 0) {
+                return dataDiscounts.length-1
+            }
+            return discount
+        }))
+    }
+    
+    const next = () => {
+        setDiscount((discount => {
+            discount ++
+                if(discount > dataDiscounts.length-1) {
+                    discount = 0
+                }
+                return discount
+            }))
+    }
+
     return(
-        <div>
-            <h1>СКИДКИ</h1>
+        <div className='container-discounts'>
+            <div className='bannerHomePage margin'>
+                <img src={banner} width='100%' alt='BEST PIZZA'/>
+            </div>
+            <div className='discounts-btn margin'>
+                <button className='btnBasket left' onClick={previos}>Назад</button>
+                <button className='btnBasket left' onClick={next}>Вперед</button>
+            </div>
+
+            <div className='discounts-block margin'>
+                <img align="center" className='imageFood' src={image} width='350px' height='480px' alt='наши акции'/>
+                <div className='discounts-text'>
+                    <h2 className='discounts-header'>{header}</h2>
+                    <p className='discounts-par'>{text}</p>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/dataDiscounts.js
@@ -0,0 +1,35 @@
+import one from './photo/action-one.jfif'
+import two from './photo/action-two.jfif'
+import three from './photo/action-three.jfif'
+import four from './photo/action-four.jfif'
+
+export const dataDiscounts = [
+    {
+        id: 1,
+        image: one,
+        header: 'Скидка 10% за отзыв',
+        text: 'Здесь будут правила акции номер 1'
+    },
+
+    {
+        id: 2,
+        image: two,
+        header: 'Акция «3+1»',
+        text: 'Здесь будут правила акции номер 2'
+    },
+
+    {
+        id: 3,
+        image: three,
+        header: 'Пицца за опоздание',
+        text: 'Здесь будут правила акции номер 3'
+    },
+
+    {
+        id: 4,
+        image: four,
+        header: 'Подарок в день рождения',
+        text: 'Здесь будут правила акции номер 4'
+    },
+
+]
